3e67eb0b442dc8231cc031436243317b80ef3d6d
Eunseong Jang <ejang21@umd.edu>
2024-10-07 17:59:47 -0400

    Afghanistan disaster

--- Food security data EDA.R
+++ Food security data EDA.R
@@ -3,16 +3,19 @@
 library(readxl)
 library(stringi)
 library(tidyverse)
+library(tidygeocoder)
 library(gridExtra)
 library(colmaps)
 library(lubridate)
+library(sf)
+library(sp)
 
 # How does the Population Tracking Tool work? (https://www.ipcinfo.org/ipc-country-analysis/population-tracking-tool/faqs/en/)
 # One of the key features of IPC analyses is that they offer data on the current food security situation in a given country,
 # but they also offer projections to give the audience an idea of how the situation could progress over time based on key drivers.
 # With this in mind, the IPC Population Tracking Tool offers data for the current, first projection and second projection, side-by-side,
 # for each country. This can be viewed by scrolling to the right of the page.
-
+{
 disaster <- read_xlsx("Food Security/public_emdat_2024-09-17.xlsx")
 FSI <- read_xlsx("Food Security/fsi-2017.xlsx")[,1:16] %>% mutate(Year = year(Year))
 for (i in 2018:2023) {
@@ -25,46 +28,63 @@ for (i in 2018:2023) {
 }
 conflict_AF <- read.csv("Food Security/Africa_conflict_1997-2024_Sep13.csv") %>% as_tibble
 conflict_LA <- read.csv("Food Security/LatinAmerica_conflict_2018-2024_Sep20.csv") %>% as_tibble
-IPC <- read_xlsx("Food Security/Acute Food Security Phase Classification (IPC) Data 2017-2024.xlsx") %>% 
-  filter(!is.na(Country)) %>% filter(!is.na(Area_id)) %>% filter(!is.na(Population)) %>% 
-  mutate()
-IPC$Area_Phase <- IPC %>% select(Phase_1, Phase_2, Phase_3, Phase_4, Phase_5) %>% apply(1, function(x) which.max(x)) %>% as.factor
-IPC$Date <- as.Date(paste(IPC$Date, "01"), format="%b %Y %d")
-IPC$Year <- year(IPC$Date) %>% as.factor
-IPC$Month <- month(IPC$Date) %>% as.factor
-IPC <- IPC %>% relocate(Country:Area_id, Year, Month)
+conflict_ME <- read.csv("Food Security/conflict_2017-01-01-2024-10-06-Caucasus_and_Central_Asia.csv") %>% as_tibble
+IPC_AF <- read_xlsx("Food Security/East and Central Africa - Acute Food Security Phase Classification (IPC) Data 2017-2024.xlsx") %>% 
+  filter(!is.na(Country)) %>% filter(!is.na(Area_id)) %>% filter(!is.na(Population))
+IPC_AF$Area_Phase <- IPC_AF %>% select(Phase_1, Phase_2, Phase_3, Phase_4, Phase_5) %>% apply(1, function(x) which.max(x)) %>% as.factor
+IPC_AF$Date <- as.Date(paste(IPC_AF$Date, "01"), format="%b %Y %d")
+IPC_AF$Year <- year(IPC_AF$Date) %>% as.factor
+IPC_AF$Month <- month(IPC_AF$Date) %>% as.factor
+IPC_AF <- IPC_AF %>% relocate(Country:Area_id, Year, Month)
 
+IPC_AS <- read_xlsx("Food Security/Asia - Acute Food Security Phase Classification (IPC) Data 2017-2024.xlsx") %>% 
+  filter(!is.na(Country)) %>% filter(is.na(Population))
+IPC_AS$Area_Phase <- IPC_AS %>%
+  select(Phase_1, Phase_2, Phase_3, Phase_4, Phase_5) %>%
+  apply(1, function(x) ifelse(sum(is.na(x)) == 5, 0, which.max(x))) %>% as.factor
+IPC_AS$Date <- as.Date(paste(IPC_AS$Date, "01"), format="%b %Y %d")
+IPC_AS$Year <- year(IPC_AS$Date) %>% as.factor
+IPC_AS$Month <- month(IPC_AS$Date) %>% as.factor
+IPC_AS <- IPC_AS %>% relocate(Country:Area_id, Year, Month)
+
+IPC_Nig <- st_read("Food Security/West Africa/WA_201101_CS.shp")
 
 disaster$`Start Year` %>% unique     # 2001~2024
 conflict_AF$year %>% unique %>% sort # 1997~2024
 conflict_LA$year %>% unique %>% sort # 2018~2024
-IPC$Year %>% unique %>% sort         # 2017~2024
+IPC_AF$Year %>% unique %>% sort         # 2017~2024
 
-IPC_Col <- IPC %>% filter(Country == "Colombia") # 0
+## FSI has 179 countries
+IPC_Col <- IPC_AF %>% filter(Country == "Colombia") # 0
 FSI_Col <- FSI %>% filter(Country == "Colombia")
 disaster_Col <- disaster %>% filter(Country == "Colombia") # 198
 conflict_Col <- conflict_LA %>% filter(country == "Colombia") # 28,230
 
-
-IPC_CAR <- IPC %>% filter(Country == "Central African Republic") # 515
+IPC_CAR <- IPC_AF %>% filter(Country == "Central African Republic") # 515
 FSI_CAR <- FSI %>% filter(Country == "Central African Republic")
 disaster_CAR <- disaster %>% filter(Country == "Central African Republic") # 66
 conflict_CAR <- conflict_AF %>% filter(country == "Central African Republic") # 7,384
 
+# no population data for Nigeria
+FSI_Nig <- FSI %>% filter(Country == "Nigeria")
+disaster_Nig <- disaster %>% filter(Country == "Nigeria") # 432
+conflict_Nig <- conflict_AF %>% filter(country == "Nigeria") # 37,979 # much more than Colombia or CAR
+
+# Afghanistan
+IPC_Afg <- IPC_AS %>% filter(Country == "Afghanistan") # 549
+FSI_Afg <- FSI %>% filter(Country == "Afghanistan")
+disaster_Afg <- disaster %>% filter(Country == "Afghanistan") # 214
+conflict_Afg <- conflict_ME %>% filter(country == "Afghanistan") # 66,500 # much more than CAR
+}
+
+# disaster_Afg %>% select(Location) %>% write.csv("Food Security/Afghanistan disaster.csv", row.names=F)
+
 disaster_Col$Location %>% head(10)
 disaster_CAR$Location %>% head(10)
+disaster_Nig$Location %>% head(10)
 
 # The Central African Republic is divided into 20 prefectures (14 administrative prefectures and 2 economic prefectures ) and one autonomous commune.
 # The prefectures (préfectures) are further divided into 84 sub-prefectures (sous-préfectures).
-tot_population_CAR <- read_xlsx("Food Security/Acute Food Security Phase Classification (IPC) Data 2017-2024.xlsx") %>%
-  filter(grepl("Central African Republic", Country) & is.na(Area_id))
-tot_population_CAR$Country <- "Central African Republic"
-tot_population_CAR$Date <- as.Date(paste(tot_population_CAR$Date, "01"), format="%b %Y %d")
-tot_population_CAR$Year <- year(tot_population_CAR$Date) %>% as.factor
-tot_population_CAR$Month <- month(tot_population_CAR$Date) %>% as.factor
-tot_population_CAR <- tot_population_CAR %>% select(Country, Year, Month, Population)
-tot_population_CAR %>% arrange(Year, Month) %>% filter(!is.na(Population))
-
 IPC_CAR %>% group_by(Year, Month) %>% summarize(n_subareas=Subarea %>% unique %>% length)
 
 IPC_CAR_national_populations <- IPC_CAR %>%
---
+++ parsing disaster locations.R
@@ -0,0 +1,67 @@
+# setwd("C:/Users/gkfrj/Documents/R")
+library(readxl)
+library(stringi)
+library(tidyverse)
+library(tidygeocoder)
+library(gridExtra)
+library(colmaps)
+library(lubridate)
+library(sf)
+library(sp)
+
+disaster <- read_xlsx("Food Security/public_emdat_2024-09-17.xlsx")
+disaster_Afg <- disaster %>% filter(Country == "Afghanistan") # 214
+disaster_Afg$Location %>% head
+disaster_Afg$Location <- gsub(";", ",", disaster_Afg$Location)
+disaster_Afg$Location <- gsub(" &", ",", disaster_Afg$Location)
+disaster_Afg %>% select(Location) %>% view
+
+loc_row6 <- (disaster_Afg$Location[6] %>% strsplit(", "))[[1]]
+
+# can we believe ArcGIS query results?
+ex1 <-  geo(address="Hilmand, Afghanistan", method = "arcgis")
+ex2 <-  geo(address=paste0(loc_row6, ", Afghanistan"), method = "arcgis")
+ex2
+ex2_rev_geocode <- reverse_geocode(ex2, lat = lat, long = long, address = addr, method = "arcgis", full_results = T) %>% view
+
+# Hindu Kush montain (Wakhan district, Badakhshan province), Souchi Bala Payan, Yawan villages (Yawan district, Badakhshan province),
+# Souch village (Jorm district, Badakhshan province), Jurm, Malang Ab villages (Baharak district, Badakhshan province), Rostaq district (Takhar province),
+# Kabul district (Kabul province), Khuram Wa Sarbagh, Aybak districts (Samagan province)
+(disaster_Afg$Location[15] %>% strsplit("\\([^)]+,(*SKIP)(*FAIL)|,\\s*", perl=T))[[1]]
+
+
+disaster_Afg %>% filter(grepl("\\(", Location)) %>% select(Location) %>% view
+disaster_Afg %>% filter(grepl("mountain", Location)) %>% select(Location)
+disaster_Afg %>% filter(grepl("montain", Location)) %>% select(Location)
+disaster_Afg %>% filter(grepl("region", Location)) %>% select(Location)
+
+disaster_Afg_separate_locations <- tibble()
+start.t <- Sys.time()
+for (i in 1:nrow(disaster_Afg)) {
+  DisNo._i <- disaster_Afg$DisNo.[i]
+  locations_split <- strsplit(disaster_Afg$Location[i], "\\([^)]+,(*SKIP)(*FAIL)|,\\s*", perl=T)[[1]]
+  locations_split <- locations_split[!(locations_split %in% c("Northern", "Southern", "Eastern", "Western"))]
+  locations_split <- sapply(locations_split, function(x) ifelse(grepl("region", x),
+                                                                substr(x,
+                                                                       gregexpr("\\(", x)[[1]][1] + 1,
+                                                                       gregexpr("\\)", x)[[1]][1] - 1),
+                                                                x))
+  locations_split <- strsplit(locations_split, "\\b, \\b|\\b and \\b")
+  ArcGIS_coords <- geo(address=paste0(locations_split, ", Afghanistan"),
+                       method = "arcgis", 
+                       unique_only = T)
+  ArcGIS_rev_geocode <- reverse_geocode(ArcGIS_coords,
+                                        lat = lat, 
+                                        long = long, 
+                                        address = addr,
+                                        method = "arcgis",
+                                        full_results = T) %>% 
+    mutate(DisNo._i=DisNo._i) %>% relocate(DisNo._i, address)
+  disaster_Afg_separate_locations <- rbind(disaster_Afg_separate_locations, ArcGIS_rev_geocode)
+  if (i %% 10 == 0) print(paste0(i, "th row complete"))
+}
+end.t <- Sys.time()
+end.t - start.t # 14.13823 mins
+disaster_Afg_separate_locations
+disaster_Afg_separate_locations %>% view
+# disaster_Afg_separate_locations %>% write.csv("Food Security/Disaster Afghanistan locations.csv", row.names=F)
